f19ac529e85c4d1e45f4ee30fb4c5b4c5ca7b4b2
6vsk.viesis1 <6vsk.viesis1@ventspils.edu.lv>
2025-01-15 15:36:03 +0200

    changed sounds

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 class Dzivnieks:
-    def __init__(self, name, kajas, sound):
+    def __init__(self, name, kajas, species):
         self.name = name
         self.kajas = kajas
-        self.sound = sound
+        self.species = species
+        if species == "dog":
+            self.sound = "WOOF"
+        elif species == "cat":
+            self.sound = "MEOW!"
+        elif species == "bird":
+            self.sound = "CAWW!"
+        else:
+            self.sound = "Animal Noise"
 
     def skanja(self):
         return self.sound
 
-tigeris = Dzivnieks("tigeris", 4, "meow in caps")
+
+tigeris = Dzivnieks("tigeris", 4, "big cat")
 print(tigeris.skanja())

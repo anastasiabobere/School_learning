4ba2da3a3cf7b70800b1f54a6e452f3d205f0db9
6vsk.viesis1 <6vsk.viesis1@ventspils.edu.lv>
2025-01-16 10:36:39 +0200

    1 uzdevums 3 dala

---
+++ exams/exam23.py
@@ -0,0 +1,22 @@
+class Doctorate:
+    def __init__(self, name="N/A", pacientuSkaits=0):
+        self.name= name
+        self.pacientuSkaits = pacientuSkaits
+    def ievads(self):
+        self.name= input("Ievadiet doktorāta nosaukumu: ")
+        try:
+            self.pacientuSkaits=int( input("Ievadiet pacientu skaitu: "))
+        except:
+            self.pacientuSkaits =0
+            print("Ievadiet veselus skaitlus")
+        # finally:
+        #     print("ievade veiksmiga", self.pacientuSkaits)    
+    def izvade(self):
+        addition =""
+
+        if(self.pacientuSkaits%10 !=1):
+            addition ="s"
+        print( f"Doktorāts {self.name} apkalpo {self.pacientuSkaits} pacientu{addition}.")
+d1 = Doctorate()
+d1.ievads()
+d1.izvade()

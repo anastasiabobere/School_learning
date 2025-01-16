ac01bc502567f76b602b1eda8c82a7b286de04bb
6vsk.viesis1 <6vsk.viesis1@ventspils.edu.lv>
2025-01-16 09:08:36 +0200

    added list

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class Dzivnieks:
     def skanja(self):
         return self.sound
     def __str__(self):
-        return f"{self.name} un vinam ir {self.kajas} kajas"
+        return f" Tas ir {self.name} un vinam ir {self.kajas} kajas"
 
 class Suns(Dzivnieks):
     def __init__(self, name, kajas):
@@ -22,3 +22,6 @@ class Kakis(Dzivnieks):
 s1 = Suns("Arnold", 4)
 d1 = Dzivnieks("Gauja", 6)
 print(d1)
+dzivnieki =[Kakis("Cat1", 4),Kakis("Cat2", 7), Suns("Suns1", 3), Dzivnieks("Arnolds", 9)]
+for animal in dzivnieki:
+    print(animal)

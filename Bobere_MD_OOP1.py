c39d890cdcbe77dcf8b174f6cea1b07f155a9e4b
Anastasia Bobere <anastasia.bobere@gmail.com>
2025-01-29 15:01:45 +0200

    modifed 1st

--- Bobere_MD_OOP1.py
+++ Bobere_MD_OOP1.py
@@ -1,10 +1,15 @@
 class Turnirs :
-    def __init__(self, cilveku_skaits,grupu_skaits,sporta_veids):
-        self.nosaukums=""
+    nosaukums = "N/A"
+    cilveku_skaits =0
+    grupu_skaits = 0
+    sporta_veids= "N/A"
+    
+    def __init__(self,nosaukums,  cilveku_skaits,grupu_skaits,sporta_veids):
+        self.nosaukums= nosaukums
         self.cilveku_skaits=cilveku_skaits
         self.grupu_skaits=grupu_skaits
         self.sporta_veids=sporta_veids
-        self.sponsori=[]
+        self.sponsori = []
     def pievenot_sponsoru(self,sponsor):
         self.sponsori.append(sponsor)
     def izvade(self):
@@ -12,14 +17,14 @@ class Turnirs :
         print("Sponsori:")
         for sponsor in self.sponsori:
             print("  ", sponsor)
-turnīrs1 = Turnirs(18, 5, "Fortnite")
-turnīrs1.nosaukums = "Last Dab 2025"
+turnīrs1 = Turnirs("Last Dab 2023",18, 5, "Fortnite")
+# turnīrs1.nosaukums = "Last Dab 2025"
 turnīrs1.pievenot_sponsoru("Adidaš")
 turnīrs1.pievenot_sponsoru("Mike")
 turnīrs1.pievenot_sponsoru("DolčeNKabana")
 
-turnīrs2 = Turnirs(24, 6, "Basketbols")
-turnīrs2.nosaukums = "Urban Basket Challenge"
+turnīrs2 = Turnirs("Urban Basket Challenge",24, 6, "Basketbols")
+# turnīrs2.nosaukums = "Urban Basket Challenge"
 turnīrs2.pievenot_sponsoru("Nike")
 turnīrs2.pievenot_sponsoru("V6VSK")
 turnīrs2.pievenot_sponsoru("Red Bull")

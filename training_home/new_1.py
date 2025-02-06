94ba859776b0dd9d87f95dcc4500ed5fbac6b316
Anastasia Bobere <anastasia.bobere@gmail.com>
2025-02-06 08:31:47 +0200

    cleaned

--- training_home/new_1.py
+++ training_home/new_1.py
@@ -4,19 +4,18 @@ class Video_Game:
         self.genre = genre
         self.platform = platform
         self.multiplayer = True
-        self.ratings = []  # Store as a list
+        self.ratings = []
 
     def add_rating(self, rating):
         self.ratings.append(rating)
 
     def toggle_Multiplayer(self):
-        self.multiplayer = not self.multiplayer  # Cleaner way
+        self.multiplayer = not self.multiplayer 
 
     def __str__(self):
         return f'Spēle {self.title} ir {self.genre} žanra {"multiplayer" if self.multiplayer else "single-player"} spēle uz {self.platform}. Atsauksmes: {", ".join(self.ratings) if self.ratings else "Nav atsauksmju"} '
 
 game = Video_Game("Minecraft", "survival", "PC")
-game.toggle_Multiplayer()
 game.add_rating("Forši")
 game.add_rating("Aizraujoši")
 game.add_rating("Not good")
